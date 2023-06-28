03fbf02a14cd7479e868534202dd563e9c608fb2
sarath2707 <sarathreddy.polu@gamail.com>
2023-06-28 18:54:15 +0100

    pushing the react microservices

---
+++ react-microservice/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <p>This is a basic React application.</p>
+    </div>
+  );
+}
+
+export default App;
+
